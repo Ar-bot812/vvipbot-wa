d232730faf07c29cb436363f2c08f9fcde3b9233
Ar-bot812 <80480623+Ar-bot812@users.noreply.github.com>
2021-03-26 02:48:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ const menu = require("./lib/menu.js")
 
 //Setting
 
-const apivhtear = 'Apikey vhtear';
-const apibarbar = 'Apikey mhankbarbar';
+const apivhtear = 'Apikey Aziz';
+const apibarbar = 'Apikey Aziz';
 const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const instagram = 'wolf_nation_rmx- kaelMk_'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FT:Aziz\n' // Aziz
+            + 'ORG:VVIPINDO;\n' // Ar Bot
+            + 'TEL;type=CELL;type=VOICE;waijd=6288238713432:+62 88238713432\n' //088238713432
             + 'END:VCARD'
 const
 {
